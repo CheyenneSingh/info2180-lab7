34f7174cc13d0cbe6290a9314a6514d8a3e9009b
Ubuntu <ubuntu@ip-172-31-40-204.us-east-2.compute.internal>
2019-11-21 06:52:23 +0000

    Exercise 5

--- world.php
+++ world.php
@@ -6,19 +6,63 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $country = ucfirst(strtolower($_REQUEST["country"]));
-
+$context = $_REQUEST["context"];
 
 if ($country == ""){
 	$stmt = $conn->query("SELECT * FROM countries");
 	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}elseif($context !=""){
+	$stmt = $conn->prepare("SELECT code FROM countries WHERE name LIKE '%$country%'");
+	$stmt->execute();
+	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	$code = $results[0]['code'];
+	$newstmt =$conn->prepare("SELECT * FROM cities WHERE country_code LIKE '%$code%'");
+	$newstmt->execute();
+	$results = $newstmt->fetchAll(PDO::FETCH_ASSOC);
+	
 }else{
 	$stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE '%$country%'");
 	$stmt->execute();
 	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 }
+ ?>
+
+<?php if(empty($context)){
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+    <table>
+		<thead>
+			<tr class="heading">
+				<th >Name</th>
+				<th >Continent</th>
+				<th >Independence</th>
+				<th >Head of State</th>
+			</tr>
+		</thead>
+		<?php foreach ($results as $row): ?>
+			<tr class="row">
+				<td  ><?= $row['name']; ?></td>
+				<td  ><?= $row['continent']; ?></td>
+				<td  ><?= $row['independence_year']; ?></td>
+				<td  ><?= $row['head_of_state'];?></td>
+			</tr>
+		<?php endforeach; ?>
+	</table>
+<?php }else{ ?>
+    <table>
+		<thead>
+			<tr class="heading">
+				<th >Name</th>
+				<th >District</th>
+				<th >Population</th>
+			</tr>
+		</thead>
+		<?php foreach ($results as $row): ?>
+			<tr class="row">
+				<td  ><?= $row['name']; ?></td>
+				<td  ><?= $row['district']; ?></td>
+				<td  ><?= $row['population']; ?></td>
+			</tr>
+		<?php endforeach; ?>
+	</table>
+<?php } ?>
